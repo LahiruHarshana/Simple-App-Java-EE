60f0b6f02ac3e3ed01bc6ad621edb12c7349bcf7
LahiruHarshana <lharshana2002@gmail.com>
2023-11-30 11:34:47 +0530

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-public class Main {
+import javax.servlet.Servlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "/customer")
+public class Main extends HttpServlet {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
