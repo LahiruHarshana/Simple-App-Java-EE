672bf6395011de9649a3bcc699a6a74cebac263d
LahiruHarshana <lharshana2002@gmail.com>
2023-11-30 11:35:48 +0530

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 import javax.servlet.Servlet;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(urlPatterns = "/customer")
 public class Main extends HttpServlet {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Customer Saved");
     }
 }
